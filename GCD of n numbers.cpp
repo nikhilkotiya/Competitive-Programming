e92854bc5dbdbe165d1c2c6732ca27aed49f0ab3
nikhilkotiya <nikhilkotiya8@gmail.com>
2021-02-26 16:14:50 +0530

    GCD

---
+++ GCD of n numbers.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+const int a=1e5+10;
+int forw[a];
+int back[a];
+int main() {
+    int t,l,r;
+    cin>>t;
+    while(t--)
+    {
+        int n,q;
+        cin>>n>>q;
+        for(int i=1;i<=n;i++)
+        {
+            cin>>arr[i];
+            arr[i]=__gcd(gc,arr[i]);
+        }
+        
+        for(int z=1;z<=n;z++)
+        {
+            
+        }
+        
+        
+        
+        int gc=0;
+        for(int i=1;i<=l-1;i++)
+        {
+            gc=__gcd(gc,arr[i]);
+        }
+        
+        for(int i=r+1;i<=n;i++)
+        {
+            gc=__gcd(gc,arr[i]);
+        }
+        
+        
+        while(q--)
+        {
+            cin>>l>>r;
+                
+        }
+    }
+}
