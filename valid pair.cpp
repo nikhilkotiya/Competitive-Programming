ffe3e84b34403be216302cb5cc7a94480b64d2d7
nikhilkotiya <nikhilkotiya8@gmail.com>
2021-04-03 15:36:47 +0530

    valid pair

---
+++ valid pair.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,c;
+	cin>>a>>b>>c;
+	if(a==b)
+	{
+	   cout<<"YES";
+	}
+	else if(b==c)
+	{
+	    cout<<"YES";
+	}
+	else if(c==a)
+	{
+	    cout<<"YES";
+	}
+	else
+	
+	{
+	    cout<<"NO";
+	}
+	return 0;
+}
